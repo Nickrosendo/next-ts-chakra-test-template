e6ec14dd46c99328ed750caee8900e41f95af706
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2022-05-16 20:52:32 -0300

    fix: include configuration on chakra-provider to support ssr colormode caching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,33 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  cookieStorageManager,
+  localStorageManager,
+} from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 import "@root/styles/globals.css";
 
 import { defaultTheme } from "@root/themes";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, cookies }: AppProps) {
+  const colorModeManager =
+    typeof cookies === "string"
+      ? cookieStorageManager(cookies)
+      : localStorageManager;
+
   return (
-    <ChakraProvider theme={defaultTheme}>
+    <ChakraProvider theme={defaultTheme} colorModeManager={colorModeManager}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
 }
 
 export default MyApp;
+
+export function getServerSideProps({ req }) {
+  return {
+    props: {
+      cookies: req.headers.cookie ?? "",
+    },
+  };
+}
