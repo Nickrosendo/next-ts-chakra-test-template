fe1dbc5d7db4202896900af240c6330401e2330d
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2022-05-16 21:09:17 -0300

    fix: chakra-provider cookie configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,9 @@
-import {
-  ChakraProvider,
-  cookieStorageManager,
-  localStorageManager,
-} from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 import "@root/styles/globals.css";
 
-import { defaultTheme } from "@root/themes";
-
-function MyApp({ Component, pageProps, cookies }: AppProps) {
-  const colorModeManager =
-    typeof cookies === "string"
-      ? cookieStorageManager(cookies)
-      : localStorageManager;
-
-  return (
-    <ChakraProvider theme={defaultTheme} colorModeManager={colorModeManager}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
-
-export function getServerSideProps({ req }) {
-  return {
-    props: {
-      cookies: req.headers.cookie ?? "",
-    },
-  };
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,39 @@
-import { Heading, Box } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  cookieStorageManager,
+  localStorageManager,
+  Heading,
+  Box,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+import { defaultTheme } from "@root/themes";
+
+const Home: NextPage = ({ cookies }) => {
+  const colorModeManager =
+    typeof cookies === "string" ? cookieStorageManager : localStorageManager;
+
   return (
-    <Box
-      role="main"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      h="100vh"
-    >
-      {" "}
-      <Heading textAlign="center"> hello world </Heading>
-    </Box>
+    <ChakraProvider theme={defaultTheme} colorModeManager={colorModeManager}>
+      <Box
+        role="main"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        h="100vh"
+      >
+        <Heading textAlign="center"> hello world </Heading>
+      </Box>
+    </ChakraProvider>
   );
 };
 
 export default Home;
+
+export function getServerSideProps({ req }) {
+  return {
+    props: {
+      cookies: req.headers.cookie ?? "",
+    },
+  };
+}
